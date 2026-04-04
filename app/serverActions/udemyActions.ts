17e1f48443267954099ff7f73632e4127e3afe9c
Nicolas Coussot <lpdmots@gmail.com>
2026-04-04 16:15:05 +0200

    fix(cache): remove conflicting fetch cache option for Udemy

--- app/serverActions/udemyActions.ts
+++ app/serverActions/udemyActions.ts
@@ -7,7 +7,6 @@ export const getCourseDetails = async (courseId: string) => {
     try {
         const response = await fetch(`https://www.udemy.com/api-2.0/courses/${courseId}/?fields[course]=num_subscribers,num_reviews,avg_rating`, {
             method: "GET",
-            cache: "force-cache",
             next: { revalidate: UDEMY_CACHE_SECONDS, tags: [`udemy:course:${courseId}`] },
             headers: {
                 Accept: "application/json, text/plain, */*",
@@ -30,7 +29,6 @@ export const getLastComments = async (courseId: string, page: number) => {
     try {
         const response = await fetch(`https://www.udemy.com/api-2.0/courses/${courseId}/reviews/?page=${page.toString()}&page_size=50&fields[results]=content,rating,user,created`, {
             method: "GET",
-            cache: "force-cache",
             next: { revalidate: UDEMY_CACHE_SECONDS, tags: [`udemy:course:${courseId}:reviews:${page}`] },
             headers: {
                 Accept: "application/json, text/plain, */*",
